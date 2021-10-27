21306980be3ee7fd123f3551be164487224edcb2
Marija <marijap@protonmail.com>
2021-10-27 23:36:15 +0200

    Add initial spring boot application

---
+++ fishingbooker/src/main/java/com/example/fishingbooker/FishingbookerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishingbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingbookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingbookerApplication.class, args);
+	}
+
+}
