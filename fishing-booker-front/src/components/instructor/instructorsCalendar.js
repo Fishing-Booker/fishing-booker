fa2c9c4f2dc0bfd62ff2c6db4606ea36558846c7
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-01-14 14:04:07 +0100

    feat: show periods in calendar

--- fishing-booker-front/src/components/instructor/instructorsCalendar.js
+++ fishing-booker-front/src/components/instructor/instructorsCalendar.js
@@ -6,47 +6,48 @@ import Calendar from 'react-awesome-calendar';
 import {useParams }from 'react-router'
 import AddInstructorsAvailablePeriod from "./addInstructorsAvailablePeriod"
 import axios from 'axios';
+import { format } from 'date-fns';
 
 const InstructorsCalendar = () => {
     const [addPeriod, setAddPeriod] = useState(false);
     const {adventureId} = useParams();
     const [periods, setPeriods] = useState([]);
     const SERVER_URL = process.env.REACT_APP_API; 
+    //const [events, setEvents] = useState([]);
 
     const colors = ['#fd3153', '#1ccb9e', '#3694DF', '#00008B', '#8B008B', '#FF8C00', '#00CED1', '#DAA520']
 
     useEffect(() => {
+        setPeriods([])
         const headers = {'Content-Type' : 'application/json', 'Authorization' : `Bearer ${localStorage.jwtToken}`}
 
         axios.get(SERVER_URL + "/ownerPeriods/getOwnerReservationPeriods", {headers: headers})
           .then(response => {
-            setPeriods(response.data);
-            console.log(response.data)
+            var dates = response.data;
+            for(let dat of dates) {
+                dat.startDate = format(dat.startDate, 'yyyy-MM-dd kk:mm');
+                dat.endDate = format(dat.endDate, 'yyyy-MM-dd kk:mm');
+            }
+            console.log(dates)
+
+            for(let dat of dates){
+                var event = {
+                    color: colors[Math.floor(Math.random()*colors.length)],
+                    from: dat.startDate,
+                    to: dat.endDate
+                }
+                periods.push(event);
+            }
+            console.log(periods)
+            setPeriods(periods);
         });
     }, [])
-
-    const events = [{
-        color: colors[Math.floor(Math.random()*colors.length)],
-        from: '2022-01-02T00:00:00',
-        to: '2022-01-04T19:00:00',
-        title: 'FREE'
-    }, {
-        color: '#1ccb9e',
-        from: '2022-01-03T00:00:00+00:00',
-        to: '2022-01-06T19:00:00+00:00',
-        title: 'This is another event'
-    }, {
-        color: '#3694DF',
-        from: '2022-01-05T13:00:00+00:00',
-        to: '2022-01-05T20:00:00+00:00',
-        title: 'This is also another event'
-    }];
 
     return(
         <div className="calendar-wrapper">
             <button className="add-period-btn" onClick={() => setAddPeriod(true)}>+</button>
             <Calendar 
-                events={events}
+                events={periods}
             />
             <AddInstructorsAvailablePeriod modalIsOpen={addPeriod} setModalIsOpen={setAddPeriod}/>
         </div>
