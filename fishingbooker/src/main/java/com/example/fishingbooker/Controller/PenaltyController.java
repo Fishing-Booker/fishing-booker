2317b57a8c24d7e06e68a829b59cdb6a332aeb50
marija.petrovic <marijap@protonmail.com>
2022-03-03 17:17:34 +0100

    annul penalties every month

--- fishingbooker/src/main/java/com/example/fishingbooker/Controller/PenaltyController.java
+++ fishingbooker/src/main/java/com/example/fishingbooker/Controller/PenaltyController.java
@@ -3,7 +3,9 @@ package com.example.fishingbooker.Controller;
 import com.example.fishingbooker.IService.IPenaltyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +20,11 @@ public class PenaltyController {
     @GetMapping()
     public Integer getClientPenalties(@RequestParam Integer clientId) {
         return penaltyService.findClientPenalties(clientId);
+    }
+
+    @PutMapping()
+    public ResponseEntity<String> annulPenalties(@RequestBody Integer clientId) {
+        penaltyService.annulPenalties(clientId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
