d7cc920b4229449004664fa57000fea97f892adb
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-05-29 15:25:13 +0200

    fix

--- fishingbooker/src/main/java/com/example/fishingbooker/config/WebCongif.java
+++ fishingbooker/src/main/java/com/example/fishingbooker/config/WebCongif.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebCongif implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://fishing-booker-frontend.herokuapp.com").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://fishing-booker-frontend.herokuapp.com", "http://fishing-booker-frontend.herokuapp.com").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
