57ee21293628592581a583af8caa98cde2bf8b62
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-05-29 12:22:10 +0200

    update web config

--- fishingbooker/src/main/java/com/example/fishingbooker/config/WebSecurityConfig.java
+++ fishingbooker/src/main/java/com/example/fishingbooker/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.POST, "/auth/login", "/auth/register");
-        web.ignoring().antMatchers(HttpMethod.GET,  "/webjars/**", "/*.html", "/**/*.html",
+        web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "favicon.ico", "/**/*.html",
                 "/**/*.css", "/**/*.js");
     }
 }
